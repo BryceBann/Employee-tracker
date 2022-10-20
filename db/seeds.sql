ffe223fd186936a12c38524dfc239bc8526aedd2
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-10-19 20:45:25 -0500

    small work on seed.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department(department_name)
+VALUES
+('Receiving'),
+('Testing'),
+('Shipping'),
+('Inventory');
+
+INSERT INTO employee_roles
--- db/team.sql
+++ db/team.sql
@@ -30,4 +30,6 @@ manager_id INT UNSIGNED,
 FOREIGN KEY(manager_id)
 REFERENCES(employee(id))
 ON DELETE SET NULL
-)
+);
+USE team_db;
+
