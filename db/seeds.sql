f55fdac48ba7e3f8bc1e8aa8c370b09bc1dde901
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-10-24 18:28:50 -0500

    work on last set of questions and updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,26 +9,26 @@ VALUES
 INSERT INTO employee_roles
 (title, salary, department_id)
 VALUES
+('Receiving manager', 75000, 1),
 ('Receiver', 50000, 1),
-('Receiving Manger', 75000, 1),
+('Testing manager', 80000, 2),
 ('Tester', 55000, 2),
-('Testing Manager', 80000, 2),
-('Shipper', 50000, 3),
-('Bulk shipper', 50000, 3),
 ('Shipping Manager', 80000, 3),
-('Inventory control', 55000, 4),
-('Inventory Manager', 80000, 4 );
+('Bulk shipper', 50000, 3),
+('Shipper', 50000, 3),
+('Inventory manager', 80000, 4),
+('Inventory control', 55000, 4 );
 
 INSERT INTO employee
 (first_name, last_name, role_id, manager_id)
 VALUES
-('Name1', 'Name1', 1, NULL),
-('Name2', 'Name2', 2, 1),
-('Name3', 'Name3', 3, NULL),
-('Name4', 'Name4', 4, 3),
-('Name5', 'Name5', 5, NULL),
-('Name6', 'Name6', 6, NULL),
-('Name7', 'Name7', 7, 5),
-('Name8', 'Name8', 8, NULL),
-('Name9', 'Name9', 9, 8);
+('Jeff', 'Stevens', 1, NULL),
+('Ashley', 'Jones', 2, 1),
+('Justin', 'Thompson', 3, NULL),
+('Cody', 'Ledbetter', 4, 3),
+('Josh', 'Jacobs', 5, NULL),
+('Nicole', 'Little', 6, 5),
+('Brain', 'Wells', 7, 5),
+('Sarah', 'Thompson', 8, NULL),
+('Zach', 'Stevens', 9, 8);
 
